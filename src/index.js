4b9f0f3e4a1a4de7b63fac7065d18f7eed6c9abe
mshafivk <mshafivk@gmail.com>
2021-01-14 12:47:30 +0530

    removed test component from index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-export { default as Test } from "./components/Test";
-export { default as FormattedDate } from "./components/FormattedDate";
+export { default as FormattedDate } from './components/FormattedDate';
