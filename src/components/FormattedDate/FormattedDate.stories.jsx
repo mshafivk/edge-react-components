64d1505b0104f28f8d951757d51c4c793aebce72
mshafivk <mshafivk@gmail.com>
2021-01-20 21:09:15 +0530

    storybook updated for FormattedDate

--- src/components/FormattedDate/FormattedDate.stories.jsx
+++ src/components/FormattedDate/FormattedDate.stories.jsx
@@ -1,55 +1,34 @@
-import React from "react";
-import FormattedDate from "./FormattedDate";
+import React from 'react';
+import FormattedDate from './FormattedDate';
 
 export default {
-  title: "FormattedDate",
+  title: 'FormattedDate',
   component: FormattedDate,
 };
 
-const Tpl = () => <FormattedDate>2021-01-09T01:11:21Z</FormattedDate>;
-
-const tableStyle = {
-  borderCollapse: "collapse",
-  border: "1px solid",
-};
-const rowStyle = {
-  border: "1px solid",
-};
-const CustomTpl = () => (
-  <table cellPadding="2" border="1" style={tableStyle}>
-    <thead>
-      <tr>
-        <th>Format</th>
-        <th>Example</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr style={rowStyle}>
-        <td>fullDateTime</td>
-        <td>
-          <FormattedDate format={"fullDateTime"}>
-            2021-01-09T01:11:21Z
-          </FormattedDate>
-        </td>
-      </tr>
-      <tr style={rowStyle}>
-        <td>dateOnly</td>
-        <td>
-          <FormattedDate format={"dateOnly"}>
-            2021-01-09T01:11:21Z
-          </FormattedDate>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+const Tpl = (args) => (
+  <div>
+    {true}
+    <FormattedDate {...args}>{args.children}</FormattedDate>
+  </div>
 );
 
-export const CustomTemplate = CustomTpl.bind({});
-
-export const FormattedDateDefault = Tpl.bind({});
-FormattedDateDefault.args = {
-  format: "MMM DD YYYY HH MM SS",
+export const FullDateTime = Tpl.bind({});
+FullDateTime.args = {
+  format: 'fullDateTime',
+  children: '2021-01-09T01:11:21Z',
 };
 
-CustomTemplate.storyName = "Custom Formats";
-FormattedDateDefault.storyName = "Default";
+export const DateOnly = Tpl.bind({});
+
+DateOnly.args = {
+  format: 'dateOnly',
+  children: '2021-01-09T01:11:21Z',
+};
+
+export const FromNow = Tpl.bind({});
+
+FromNow.args = {
+  format: 'fromNow',
+  children: '2021-01-10T01:11:21Z',
+};
